c356b79a7a9f60f40a20c87269926a990e659fa0
Antti Mattila <antti.mattila@futurice.com>
2016-09-03 13:19:51 +0300

    Make noise

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Hypnoria.js
 
 Bleeps and boops via websockets!
+
+## Usage
+
+- `npm install`
+- `npm start`
+- `open http://localhost:3000` for the client
+- `open http://localhost:3000/player` for the player
--- index.js
+++ index.js
@@ -1,21 +1,24 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
-app.get('/', function(req, res) {
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
 
-app.get('/player', function(req, res) {
+app.get('/player', (req, res) => {
   res.sendFile(__dirname + '/public/player.html');
 });
 
-io.on('connection', function(socket) {
-  socket.on('sound-request', function(payload) {
+io.on('connection', socket => {
+  socket.on('sound-request', payload => {
     io.emit('sound-play', payload);
   });
 });
 
-http.listen(3000, function() {
+http.listen(3000, () => {
   console.log('listening on *:3000');
 });
